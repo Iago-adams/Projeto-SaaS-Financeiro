8fed581808beaefe2f2dd4d57f63eeaa43a3a011
andre-holler <andreluizholler@gmail.com>
2025-10-15 12:19:03 -0300

    login e logout no auth_bp

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,11 +1,30 @@
-from flask import Blueprint
+from flask import Blueprint, flash, redirect, render_template, url_for
 from app.models import User
-from forms import LoginForm
+from .forms import LoginForm
+from flask_login import login_user, logout_user, login_required
 
 auth_bp = Blueprint('auth', __name__, url_prefix='/auth')
 
+#Rota de login de usuário
 @auth_bp.route('/', methods=['GET', 'POST'])
 def login():
+    form = LoginForm()
 
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).scalar()
+
+        if user:
+            if user.password_check(form.password.data):
+                login_user(user)
+                flash(f'Bem vindo {user.username}', 'success')
+                return redirect(url_for('main.homepage'))
+        else:
+            flash('Credenciais incorretas', 'danger')
     
-    return
+    return render_template('login.html', form=form)
+
+@login_required
+@auth_bp.route('/logout/')
+def logout():
+    logout_user()
+    return redirect(url_for('main.homepage'))
