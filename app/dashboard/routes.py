7c698f4bd894455e8923d72258461c5f3b87bb62
IAGO DORIGON ADAMS <iago_adams@estudante.sesisenai.org.br>
2025-10-28 12:33:19 -0300

    API configurada

--- app/dashboard/routes.py
+++ app/dashboard/routes.py
@@ -11,17 +11,14 @@ dashboard_bp = Blueprint(
 
 #Rota de login de usuário
 @dashboard_bp.route('/', methods=['GET', 'POST'])
-@login_required
 def dashboard():
     #Insira as credenciais do usuario aqui, futuramente aqui estara a função de descriptografar e puxar do banco de dados, no momento só estão Strings quaisquer para teste com a API mock
     client_id = 'clientIDtest'
     client_secret = 'clientSECRETtest'
     
     #Usando a função para requerir o token, futuramente adicionar memoria em cache para armazenar o token momentaneamente
-    token = get_token(client_id, client_secret)
-    
+    token = get_token(client_id=client_id, client_secret=client_secret)
     #usando a função que requere os dados em JSON da API, futuramente adicinar uma tabela no banco de dados para salvar o JSON em multi-tenant, para não requisitar todas as vezes
-    #id_client = 'conta-tenant-01'
-    id_client = 'conta-tenant-02'
-    data = get_extract_data(token, id_client)
-    return render_template('dashboard.html', data=data)
+    acount_id = 'conta-tenant-01'
+    data = get_extract_data(acess_token=token, acount_id=acount_id)
+    return render_template('dashboard.html')
