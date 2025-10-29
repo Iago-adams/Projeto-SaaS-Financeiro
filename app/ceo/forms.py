79c1dac3babdd29d0bd58ff316ca201ec69c998c
andre-holler <andreluizholler@gmail.com>
2025-10-29 12:27:26 -0300

    precisa trabalhar na lógica do roleform e memberform

--- app/ceo/forms.py
+++ app/ceo/forms.py
@@ -2,7 +2,10 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, SelectField
 from wtforms.validators import DataRequired, Email
 
+class RoleForm(FlaskForm):
+    name = StringField('Nome da função', validators=[DataRequired()])
+
 class MemberForm(FlaskForm):
     username = StringField('Nome de usuário', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    role = StringField
+    role = SelectField('Função', choices=[], coerce=int)
