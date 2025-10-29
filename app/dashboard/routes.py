6149f0d9378446eb7a1d7992fe858b0f63204e0a
IAGO DORIGON ADAMS <iago_adams@estudante.sesisenai.org.br>
2025-10-29 11:39:14 -0300

    Tratados os dados da API de extrato, adicionado a biblioteca plotly

--- app/dashboard/routes.py
+++ app/dashboard/routes.py
@@ -3,6 +3,7 @@ from flask_login import login_user, logout_user, login_required
 from ..services.extract_api import get_token, get_extract_data
 from ..decorators import ceo_required
 import pandas as pd
+import json
 
 dashboard_bp = Blueprint(
     'dashboard', 
@@ -21,6 +22,11 @@ def dashboard():
     token = get_token(client_id=client_id, client_secret=client_secret)
     #usando a função que requere os dados em JSON da API, futuramente adicinar uma tabela no banco de dados para salvar o JSON em multi-tenant, para não requisitar todas as vezes
     acount_id = 'conta-tenant-01'
-    data = get_extract_data(acess_token=token, acount_id=acount_id)
-    
+    extract_API_data = get_extract_data(acess_token=token, acount_id=acount_id)
+    exctract_data_normalazed = pd.json_normalize(
+        extract_API_data,
+        record_path='transactions'
+    )
+    exctract_data_normalazed['completedAt'] = pd.to_datetime(exctract_data_normalazed['completedAt'])
+    print(exctract_data_normalazed)
     return render_template('dashboard.html')
